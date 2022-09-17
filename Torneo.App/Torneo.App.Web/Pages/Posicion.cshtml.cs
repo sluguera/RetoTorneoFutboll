572a2e30ab195f5b80b3b0741799b9ccf6fbaed8
DESKTOP-4RG7PHA\DELL LATITUDE 5420 <ladybotina88@gmail.com>
2022-09-17 00:42:44 -0500

    ajustes a cada pagina de la entidad

---
+++ Torneo.App/Torneo.App.Web/Pages/Posicion.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Torneo.App.Web.Pages
+{
+    public class PosicionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
