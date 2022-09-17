9d74b736abe884db4fd206b6210e261677f21568
DESKTOP-4RG7PHA\DELL LATITUDE 5420 <ladybotina88@gmail.com>
2022-09-16 22:56:28 -0500

    preimeros ajustes a la pagina

---
+++ Torneo.App/Torneo.App.Web/Pages/Municipio.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Torneo.App.Web.Pages
+{
+    public class MunicipioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
